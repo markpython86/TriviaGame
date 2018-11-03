a28e352439cf4c5f7f6a11a7306d8d9a20026e5b
Mark <mark.faraj86@gmail.com>
2018-11-03 08:14:31 -0500

    changing style and finalizing the issue with scoring

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,11 +7,11 @@ var timeToGuess;
 var timeTilNextQ;
 // settings
 var questionLength = 10; // seconds you have to guess
-var answerLength = 3; // seconds you're shown the answer
+var answerLength = 1; // seconds you're shown the answer
 var gameLength = 10; // number of questions per round
  function initGame(){
 	// display intro
-	$("#question").html('When you play Trivia of Thrones, you win or you die. There is no middle ground. Okay, you won\'t actually die, but some of these questions are very difficult, and you only have 10 seconds to answer, not enough time to google it! Good luck, and <em>Valar Morghulis</em>.<button id="startGame">Begin Game</button>');
+	$("#question").html('When you play Trivia of Thrones, you win or you die. Okay, you won\'t actually die, but some of these questions are very difficult, and you only have 10 seconds to answer, not enough time to google it! Good luck, and <em>Valar Morghulis</em>.<button id="startGame">Begin Game</button>');
 	$("#result").hide();
 	$("#choices li").empty();
 	$(".scoreBoard").empty();
@@ -43,17 +43,17 @@ function newQuestion(){
 function makeGuess(){
 	if ($(this).data("choice") == currentQuestion.correctAnswer){
 		numRight++;
-		showResult("Correct!");
+		showResult("Correct!", "correctResult");
 	} else {
 		numWrong++;
-		showResult("Wrong. The correct answer was " + currentQuestion.answers[currentQuestion.correctAnswer]);
+		showResult("Wrong. The correct answer was " + currentQuestion.answers[currentQuestion.correctAnswer], "wrongResult");
 	}
 }
-function showResult(msg){
+function showResult(msg, addThisClass){
 	resetTimer();
-	$("#result").html(msg).show();
+	$("#result").html(msg).show().removeClass().addClass(addThisClass);
 	setTimeout(newQuestion, answerLength*1000);
-$("#score").html("correct: " + numRight + " --- incorrect: " + numWrong);
+$("#score").html("correct: " + numRight + " <br> incorrect: " + numWrong);
 
 }
 
@@ -68,7 +68,7 @@ function showTimer(){
 function timesUp(){
 	numWrong++;
 	resetTimer();
-	showResult("Time's Up! The correct answer was " + currentQuestion.answers[currentQuestion.correctAnswer]);
+	showResult("Time's Up! The correct answer was " + currentQuestion.answers[currentQuestion.correctAnswer], "timesUp");
 }
 function resetTimer(){
 	clearInterval(timer);
@@ -81,17 +81,17 @@ function gameOver(){
 	if (score > .9){
 		praise = "Amazing! You achieved the rank of King of the Seven Kingdoms.";
 	} else if (score > .8){
-		praise = "Amazing! You achieved the rank of Shadowbinder.";
+		praise = "Amazing! You achieved the rank of Knight of the Kingsguard.";
 	} else if (score > .7){
-		praise = "Well done, you achieved the rank of Lord.";
+		praise = "Well done, you achieved the rank of Lord of Harrenhal.";
 	} else if (score > .6){
 		praise = "Well done, you achieved the rank of Maester.";
 	} else if (score > .5){
-		praise = "Well done, you achieved the rank of Squire.";
+		praise = "Well done, you achieved the rank of Knight's Squire.";
 	} else if (score > .4){
-		praise = "Not bad, you achieved the rank of Novice.";
+		praise = "Not bad, you achieved the rank of Novice of the Citadel.";
 	}
-	$("#result").html("Game Over. You got " + numRight + " questions right out of " + gameLength + ". " + praise + "<button id='newGame'>New Game</button>");
+	$("#result").removeClass().html("<h1>Game Over</h1><div class='gameOverText'>You got " + numRight + " questions right and " + numWrong + " wrong. " + praise + "</div><button id='newGame'>New Game</button>");
 	$("#newGame").on("click", initGame);
 }
 $(document).ready(initGame);
