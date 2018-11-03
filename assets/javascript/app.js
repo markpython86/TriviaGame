457d1489854d03cd6d472dcb48b12a255c08b349
Mark <mark.faraj86@gmail.com>
2018-11-03 08:51:21 -0500

    finalizing the game and adding the rest of images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,97 +1,107 @@
 //global variables
-var numRight;
-var numWrong;
-var currentQuestion;
+ var numberRight;
+var numberWrong;
+ var currentQuestion;
 var timer;
 var timeToGuess;
-var timeTilNextQ;
+var library; // a copy of the questions for this game
 // settings
 var questionLength = 10; // seconds you have to guess
 var answerLength = 3; // seconds you're shown the answer
-var gameLength = 10; // number of questions per round
+var gameLength; // set this to limit the number of questions per game
  function initGame(){
 	// display intro
-	$("#qText").html('When you play Trivia of Thrones, you win or you die. Okay, you won\'t actually die, but some of these questions are very difficult, and you only have 10 seconds to answer, not enough time to google it! Good luck, and <em>Valar Morghulis</em>.<button id="startGame">Begin Game</button>');
+	$("#qText").html('When you play Trivia of Thrones, you win or you die. Okay, so you won\'t actually die, but some of these questions are very difficult, and you only have 10 seconds to answer, not enough time to google it! Good luck, and <em>Valar Morghulis</em>.<button id="startGame">Begin Game</button>');
 	$("#result").hide();
+	$("#choices").hide();
 	$("#choices li").empty();
-	$(".scoreBoard").empty();
+	$(".scoreboard").empty();
 	//add listeners
-	$("#choices .answer").on("click", makeGuess);
-	$("#startGame").on("click", newQuestion);
+ 	$("#choices .answer").off().on("click", makeGuess);
+	$("#startGame").off().on("click", newQuestion);
 	//reset game variables
-	numWrong = 0;
-	numRight = 0;
+	numberWrong = 0;
+	numberRight = 0;
+	// creates a fresh clone of the library on each play
+	library = questLib.slice(); 
 	timeToGuess = questionLength;
+	gameLength = library.length;
 }
 function newQuestion(){
-	if(numRight+numWrong === gameLength){
+	if(numberRight + numberWrong >= gameLength){
 		gameOver();
 	} else {
 		//pick a random question that hasn't been asked already
-		var qNum = Math.floor(Math.random() * questLib.length);
-		currentQuestion = questLib[qNum];
-		questLib.splice(qNum, 1);
-		$("#result").empty().hide();
-		$("#qText").html(currentQuestion.question);
-		$("#choices").show().find(".answer").each(function(i){
-			$(this).html(currentQuestion.answers[i]);
-		});
-		// start Question Timer
-		timer = setInterval(showTimer, 1000);
-	}
-}
-function makeGuess(){
-	if ($(this).data("choice") == currentQuestion.correctAnswer){
-		numRight++;
-		showResult("Correct!", "correctResult");
-	} else {
-		numWrong++;
-		showResult("Wrong. The correct answer was " + currentQuestion.answers[currentQuestion.correctAnswer], "wrongResult");
-	}
-}
-function showResult(msg, addThisClass){
-	resetTimer();
-	$("#result").html(msg).show().removeClass().addClass(addThisClass);
-	setTimeout(newQuestion, answerLength*1000);
-$("#score").html("correct: " + numRight + " <br> incorrect: " + numWrong);
-
-}
-
-function showTimer(){
-	if (timeToGuess >= 0){
-		$("#timer").html(timeToGuess + " seconds left");
-		timeToGuess--;
-	} else {
-		timesUp();
-	}
-}
-function timesUp(){
-	numWrong++;
-	resetTimer();
-	showResult("Time's Up! The correct answer was " + currentQuestion.answers[currentQuestion.correctAnswer], "timesUp");
-}
-function resetTimer(){
-	clearInterval(timer);
-	timeToGuess = questionLength;
-	$("#timer").empty();
-}
-function gameOver(){
-	var score = (numRight/gameLength);
-	var praise = "That was pretty shabby. You get sent to the Wall.";
-	if (score > .9){
-		praise = "Amazing! You achieved the rank of King of the Seven Kingdoms.";
-	} else if (score > .8){
-		praise = "Amazing! You achieved the rank of Knight of the Kingsguard.";
-	} else if (score > .7){
-		praise = "Well done, you achieved the rank of Lord of Harrenhal.";
-	} else if (score > .6){
-		praise = "Well done, you achieved the rank of Maester.";
-	} else if (score > .5){
-		praise = "Well done, you achieved the rank of Knight's Squire.";
+		var questionNumber = Math.floor(Math.random() * library.length);
+		currentQuestion = library[questionNumber];
+		library.splice(questionNumber, 1);
+ 		resetTimer();
+ 		$("#result").empty().hide();
+ 		$("#qText").html(currentQuestion.question);
+ 		$("#choices").show().find(".answer").each(function(i){
+ 			$(this).html(currentQuestion.answers[i]);
+ 		});
+ 		$("body").css("background-image", "url('"+currentQuestion.image+"')");
+ 		// start Question Timer
+ 		timer = setInterval(showTimer, 1000);
+ 	}
+ }
+ function makeGuess(){
+ 	if ($(this).data("choice") == currentQuestion.correctAnswer){
+ 		numRight++;
+ 		showResult("Correct!", "correctResult");
+ 	} else {
+ 		numWrong++;
+ 		showResult("Wrong. The correct answer was " + currentQuestion.answers[currentQuestion.correctAnswer], "wrongResult");
+ 	}
+ }
+ function showResult(msg, addThisClass){
+ 	resetTimer();
+ 	$("#result")
+		.html(msg)
+		.show()
+		.removeClass()
+		.addClass(addThisClass);
+ 	setTimeout(newQuestion, answerLength*1000);
+ 	$("#score").html("correct: " + numberRight  + " <br> incorrect: " + numberWrong);
+ 
+ }
+ function showTimer(){
+ 	if (timeToGuess >= 0){
+ 		$("#timer").html(timeToGuess + " seconds left");
+ 		timeToGuess--;
+ 	} else {
+ 		timesUp();
+ 	}
+ }
+ function timesUp(){
+ 	numberWrong++;
+ 	resetTimer();
+ 	showResult("Time's Up! The correct answer was " + currentQuestion.answers[currentQuestion.correctAnswer], "timesUp");
+ }
+ function resetTimer(){
+ 	clearInterval(timer);
+ 	timeToGuess = questionLength;
+ 	$("#timer").empty();
+ }
+ function gameOver(){
+ 	$("body").css("background-image", 'url("assets/images/init-BG.jpg")');
+ 	var score = (numRight/gameLength);
+ 	var praise = "That was pretty shabby. You get sent to the Wall.";
+ 	if (score > .9){
+ 		praise = "Amazing! You achieved the rank of King of the Seven Kingdoms.";
+ 	} else if (score > .8){
+ 		praise = "Amazing! You achieved the rank of Knight of the Kingsguard.";
+ 	} else if (score > .7){
+ 		praise = "Well done, you achieved the rank of Lord of Harrenhal.";
+ 	} else if (score > .6){
+ 		praise = "Well done, you achieved the rank of Maester.";
+ 	} else if (score > .5){
+ 		praise = "Well done, you achieved the rank of Knight's Squire.";
 	} else if (score > .4){
 		praise = "Not bad, you achieved the rank of Novice of the Citadel.";
 	}
-	$("#result").removeClass().html("<h1>Game Over</h1><div class='gameOverText'>You got " + numRight + " questions right and " + numWrong + " wrong. " + praise + "</div><button id='newGame'>New Game</button>");
+	$("#result").removeClass().html("<h1>Game Over</h1><div class='gameOverText'>You got " + numberRight + " questions right and " + numberWrong + " wrong. " + praise + "</div><button id='newGame'>Play Again</button>");
 	$("#newGame").on("click", initGame);
 }
-$(document).ready(initGame);
+$(document).ready(initGame); 
--- assets/javascript/questions-library.js
+++ assets/javascript/questions-library.js
@@ -1,79 +1,189 @@
-var questLib = [
+var questionsLibrary = [
  	{
  	 	question: "What was Tyrion's mother's name?",
-	 	answers: ["Joanna", "Tysha", "Shiera", "No one knows."],
-	 	correctAnswer: 0,
-	 	image: "assets/images/###.png"
-	 	image: "assets/images/questionImages/000.jpg"
-	},
+ 	 	answers: ["Joanna", "Tysha", "Shiera", "No one knows."],
+ 	 	correctAnswer: 0,
+ 	 	image: "assets/images/questionImages/000.jpg"
+ 	},
+ 	{
+ 	 	question: "What was Tyrion's first wife's name?",
+ 	 	answers: ["Joanna", "Tysha", "Shiera", "Milly"],
+ 	 	correctAnswer: 1,
+ 	 	image: "assets/images/questionImages/001.jpg"
+ 	},
+ 	{
+ 	 	question: "Which of the following is not the name of one of Danaerys's dragons?",
+ 	 	answers: ["Drogon", "Viserion", "Vhagar", "Rhaegal"],
+ 	 	correctAnswer: 2,
+ 	 	image: "assets/images/questionImages/002.jpg"
+ 	},
+ 	{
+ 	 	question: "What weapon was Robert Baratheon famed for using?",
+ 	 	answers: ["A morningstar", "A sword made from a meteorite", "A warhammer", "A weirwood bow"],
+ 	 	correctAnswer: 2,
+ 	 	image: "assets/images/questionImages/003.jpg"
+ 	},
+ 	{
+ 	 	question: "Oberyn Martell's daughters are called...",
+ 	 	answers: ["The Sea Bitches", "The Desert Roses", "The Sand Snakes", "The Titan's Bastards"],
+ 	 	correctAnswer: 2,
+ 	 	image: "assets/images/questionImages/004.jpg"
+ 	},
+ 	{
+ 	 	question: "What is Oleanna Tyrell's nickname?",
+ 	 	answers: ["The Sword of the Morning", "The Queen of Thorns", "The Old Bear", "The Ghost of High Heart"],
+ 	 	correctAnswer: 1,
+ 	 	image: "assets/images/questionImages/005.jpg"
+ 	},
+ 	{
+ 	 	question: "Who burned Sandor Clegane's face?",
+ 	 	answers: ["Gregor Clegane", "King Aerys", "He did it to himself", "Ser Iln Payne"],
+ 	 	correctAnswer: 0,
+ 	 	image: "assets/images/questionImages/006.jpg"
+ 	},
+ 	{
+ 	 	question: "What was the name of Sansa Stark's direwolf?",
+ 	 	answers: ["Nymeria", "Cat", "Meraxes", "Lady"],
+ 	 	correctAnswer: 3,
+ 	 	image: "assets/images/questionImages/007.jpg"
+ 	},
+ 	{
+ 	 	question: "Who sent an assasin to kill Bran Stark in his sickbed?",
+ 	 	answers: ["His mother", "Littlefinger", "Queen Cersei", "Prince Joffrey"],
+ 	 	correctAnswer: 3,
+ 	 	image: "assets/images/questionImages/008.jpg"
+ 	},
+ 	{
+ 	 	question: "What was the real identity of the squire Arstan Whitebeard?",
+ 	 	answers: ["Rhaegar Targaryen", "Jon Connington", "Ser Barristan the Bold", "Varys"],
+ 	 	correctAnswer: 2,
+ 	 	image: "assets/images/questionImages/009.jpg"
+ 	},
+ 	{
+ 	 	question: "Why was Jorah Mormont an exile from Westeros?",
+ 	 	answers: ["He sold slaves", "He desserted from the Night's Watch", "He killed the king", "He was spying for the Targaryens"],
+ 	 	correctAnswer: 0,
+ 	 	image: "assets/images/questionImages/010.jpg"
+ 	},
+ 	{
+ 	 	question: "What is the birth order of the Lannister siblings?",
+ 	 	answers: ["Jaime, Cersei, Tyrion", "Tyrion, Cersei, Jaime", "Jaime, Tyrion, Cersei", "Cersei, Jaime, Tyrion"],
+ 	 	correctAnswer: 3,
+ 	 	image: "assets/images/questionImages/011.jpg"
+ 	},
+ 	{
+ 	 	question: "Which song was played to signal the slaughter at the Red Wedding?",
+ 	 	answers: ["The Gates of Hell", "The Shade of Asshai", "The Rains of Castamere", "The Lady Stoneheart"],
+ 	 	correctAnswer: 2,
+ 	 	image: "assets/images/questionImages/012.jpg"
+ 	},
+ 	{
+ 	 	question: "Who brought Catelyn Stark back to life?",
+ 	 	answers: ["Melisandre", "Thoros of Myr", "Jon Snow", "Beric Dondarrion"],
+ 	 	correctAnswer: 3,
+ 	 	image: "assets/images/questionImages/013.jpg"
+ 	},
+ 	{
+ 	 	question: "In the north, people ask for what to establish Guest Right?",
+ 	 	answers: ["bread and salt", "water and Wine", "pease porridge", "mac and cheese"],
+ 	 	correctAnswer: 0,
+ 	 	image: "assets/images/questionImages/014.jpg"
+ 	},
+ 	{
+ 	 	question: "What gods does Craster worship?",
+ 	 	answers: ["The Old Gods", "The Seven", "R'hllor", "The Others"],
+ 	 	correctAnswer: 3,
+ 	 	image: "assets/images/questionImages/015.jpg"
+ 	},
+ 	{
+ 	 	question: "Craster was Gilly's...",
+ 	 	answers: ["husband", "father", "neither", "both"],
+ 	 	correctAnswer: 3,
+ 	 	image: "assets/images/questionImages/016.jpg"
+ 	},
+ 	{
+ 	 	question: "At the beginning of the books, how long has it been since the last winter?",
+ 	 	answers: ["6 months", "1 year", "7 years", "35 years"],
+ 	 	correctAnswer: 2,
+ 	 	image: "assets/images/questionImages/017.jpg"
+ 	},
 	{
-	 	question: "What was Tyrion's first wife's name?",
-	 	answers: ["Joanna", "Tysha", "Shiera", "Milly"],
-	 	correctAnswer: 1,
-	 	image: "assets/images/###.png"
-	 	image: "assets/images/questionImages/001.jpg"
-	},
-	{
-	 	question: "Which of the following is not the name of one of Danaerys's dragons?",
-	 	answers: ["Drogon", "Viserion", "Vhagar", "Rhaegal"],
-	 	correctAnswer: 2,
-	 	image: "assets/images/###.png"
-	 	image: "assets/images/questionImages/002.jpg"
-	},
-	{
-	 	question: "What weapon was Robert Baratheon famed for using?",
-	 	answers: ["A morningstar", "A sword made from a meteorite", "A warhammer", "A weirwood bow"],
-	 	correctAnswer: 2,
-	 	image: "assets/images/###.png"
-	 	image: "assets/images/questionImages/003.jpg"
-	},
-	{
-	 	question: "Oberyn Martell's daughters are called...",
-	 	answers: ["The Sea Bitches", "The Desert Roses", "The Sand Snakes", "The Titan's Bastards"],
-	 	correctAnswer: 2,
-	 	image: "assets/images/###.png"
-	 	image: "assets/images/questionImages/004.jpg"
-	},
-	{
-	 	question: "What is Oleanna Tyrell's nickname?",
-	 	answers: ["The Sword of the Morning", "The Queen of Thorns", "The Old Bear", "The Ghost of High Heart"],
-	 	correctAnswer: 1,
-	 	image: "assets/images/###.png"
-	 	image: "assets/images/questionImages/005.jpg"
-	},
-	{
-	 	question: "Who burned Sandor Clegane's face?",
-	 	answers: ["Gregor Clegane", "King Aerys", "He did it to himself", "Ser Iln Payne"],
-	 	correctAnswer: 0,
-	 	image: "assets/images/###.png"
-	 	image: "assets/images/questionImages/006.jpg"
-	},
-	{
-	 	question: "What was the name of Sansa Stark's direwolf?",
-	 	answers: ["Nymeria", "Cat", "Meraxes", "Lady"],
+	 	question: "How long before the start of the books did the last dragons die?",
+	 	answers: ["3 years", "39 years", "150 years", "300 years"],
 	 	correctAnswer: 3,
-	 	image: "assets/images/###.png"
-	 	image: "assets/images/questionImages/007.jpg"
-	},
-	{
-	 	question: "Who sent an assasin to kill Bran Stark in his sickbed?",
-	 	answers: ["His mother", "Littlefinger", "Queen Cersei", "Prince Joffrey"],
-	 	correctAnswer: 3,
-	 	image: "assets/images/###.png"
-	 	image: "assets/images/questionImages/008.jpg"
-	},
-	{
-	 	question: "What was the real identity of the squire Arstan Whitebeard?",
-	 	answers: ["Rhaegar Targaryen", "Jon Connington", "Ser Barristan the Bold", "Varys"],
 	 	correctAnswer: 2,
-	 	image: "assets/images/###.png"
-	 	image: "assets/images/questionImages/009.jpg"
+	 	image: "assets/images/questionImages/018.jpg"
 	},
 	{
-	 	question: "Why was Jorah Mormont an exile from Westeros?",
-	 	answers: ["He sold slaves", "He desserted from the Night's Watch", "He killed the king", "He was spying for the Targaryens"],
-	 	correctAnswer: 0,
-	 	image: "assets/images/###.png"
-	 	image: "assets/images/questionImages/010.jpg"
-	}
-];
+ 	 	question: "Before Robert's Rebellion, Danaerys what what number in line to the Iron Throne?",
+ 	 	answers: ["1", "3", "6", "She was not in the line of succession"],
+ 	 	correctAnswer: 2,
+ 	 	image: "assets/images/questionImages/019.jpg"
+ 	},
+ 	{
+ 	 	question: "Who served the Freys pies made of their dead relatives?",
+ 	 	answers: ["Wyman Manderly", "Roose Bolton", "Maege Mormont", "Littlefinger"],
+ 	 	correctAnswer: 0,
+ 	 	image: "assets/images/questionImages/020.jpg"
+ 	},
+ 	{
+ 	 	question: "Who killed Jon Arryn, Hand of the King?",
+ 	 	answers: ["Cersei and Jaime Lannister", "Varys and Tyrion Lannister", "Littlefinger and Lysa Arryn", "Aerys Targaryen"],
+ 	 	correctAnswer: 2,
+ 	 	image: "assets/images/questionImages/021.jpg"
+ 	},
+ 	{
+ 	 	question: "What character is called 'Husband to Bears'?",
+ 	 	answers: ["Robb Stark", "Tormund Giantsbane", "Jorah Mormont", "Strong Belwas"],
+ 	 	correctAnswer: 1,
+ 	 	image: "assets/images/questionImages/022.jpg"
+ 	},
+ 	{
+ 	 	question: "How many direwolf pups did the Starks find with their dead monther?",
+ 	 	answers: ["Five", "Six", "Four", "Three"],
+ 	 	correctAnswer: 1,
+ 	 	image: "assets/images/questionImages/023.jpg"
+ 	},
+ 	{
+ 	 	question: "Who knighted The Hound?",
+ 	 	answers: ["King Robert", "King Aerys", "Ser Barristan Selmy", "No one"],
+ 	 	correctAnswer: 3,
+ 	 	image: "assets/images/questionImages/006.jpg"
+ 	},
+ 	{
+ 	 	question: "What color is Ghost, Jon's direwolf?",
+ 	 	answers: ["White", "Grey", "Black", "Red"],
+ 	 	correctAnswer: 0,
+ 	 	image: "assets/images/questionImages/024.jpg"
+ 	},
+ 	{
+ 	 	question: "Who was called 'The Beggar King'?",
+ 	 	answers: ["Robert Baratheon", "Viserys Targaryen", "Balon Greyjoy", "Renly Baratheon"],
+ 	 	correctAnswer: 1,
+ 	 	image: "assets/images/questionImages/025.jpg"
+ 	},
+ 	{
+ 	 	question: "Which of the following wasn't one of the five kings in the War of the Five Kings?",
+ 	 	answers: ["Balon Greyjoy", "Stannis Baratheon", "Viserys Targaryen", "Joffrey Baratheon"],
+ 	 	correctAnswer: 2,
+ 	 	image: "assets/images/questionImages/026.jpg"
+ 	},
+ 	{
+ 	 	question: "What was Daario Naharis before he joined Danaerys's cause?",
+ 	 	answers: ["A spy for the Lannisters", "A sellsword", "A mummer", "He never said"],
+ 	 	correctAnswer: 1,
+ 	 	image: "assets/images/questionImages/027.jpg"
+ 	},
+ 	{
+ 	 	question: "Who were Ned Stark's siblings?",
+ 	 	answers: ["Benjen, Ashara, and Peytr", "Robert and Jon", "Rickard, Lyanna, and Arya", "Brandon, Lyanna, and Benjen"],
+ 	 	correctAnswer: 3,
+ 	 	image: "assets/images/questionImages/028.jpg"
+ 	},
+ 	{
+ 	 	question: "Who won the tourney thrown for Ned Stark's appointment as Hand?",
+ 	 	answers: ["Loras Tyrell", "Gregor Clegane", "Jaime Lannister", "Sandor Clegane"],
+ 	 	correctAnswer: 3,
+ 	 	image: "assets/images/questionImages/029.jpg"
+ 	}
+ ];
